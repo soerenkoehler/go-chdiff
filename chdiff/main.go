dd4a6d2c74d2aff23384b4d0d10075bf6aa33b7b
Sören Köhler <soerenkoehler@users.noreply.github.com>
2021-02-19 02:33:01 +0100

    readable error type

--- chdiff/main.go
+++ chdiff/main.go
@@ -6,18 +6,23 @@ import (
 
 	"github.com/alecthomas/kong"
 	"github.com/soerenkoehler/chdiff-go/digest"
+	"github.com/soerenkoehler/chdiff-go/util"
 )
 
 var _Version = "DEV"
 
 var cli struct {
-	Create cmdDigest `cmd:"" name:"create" aliases:"c" help:"Create digest file for PATH"`
-	Verify cmdDigest `cmd:"" name:"verify" aliases:"v" default:"1" help:"Verify digest file for PATH"`
-	Mode   string    `name:"mode" short:"m" help:"The checksum algorithm to use [SHA256,SHA512]." enum:"SHA256,SHA512" default:"SHA256"`
+	Version cmdVersion `cmd:"" name:"version" help:"Show detailed version info."`
+	Create  cmdDigest  `cmd:"" name:"create" aliases:"c" help:"Create digest file for PATH."`
+	Verify  cmdDigest  `cmd:"" name:"verify" aliases:"v" default:"1" help:"Verify digest file for PATH."`
+}
+
+type cmdVersion struct {
 }
 
 type cmdDigest struct {
-	Path string `arg:"" name:"PATH" type:"path" default:"X" help:"Path for which to calculate the digest"`
+	Path string `arg:"" name:"PATH" type:"path" default:"." help:"Path for which to calculate the digest"`
+	Mode string `name:"mode" short:"m" help:"The checksum algorithm to use [SHA256,SHA512]." enum:"SHA256,SHA512" default:"SHA256"`
 }
 
 func main() {
@@ -29,16 +34,20 @@ func main() {
 	}
 }
 
-func doMain() []error {
+func doMain() util.ErrorList {
 	ctx := kong.Parse(&cli, kong.UsageOnError(), kong.Description("TODO: Description"))
 
 	switch ctx.Command() {
 
 	case "create", "create <PATH>":
-		return digest.Create(cli.Create.Path, "out.txt", cli.Mode)
+		return digest.Create(cli.Create.Path, "out.txt", cli.Create.Mode)
 
 	case "verify", "verify <PATH>":
-		return digest.Verify(cli.Verify.Path, "out.txt", cli.Mode)
+		return digest.Verify(cli.Verify.Path, "out.txt", cli.Verify.Mode)
+
+	case "version":
+		fmt.Fprintln(os.Stdout, _Version)
+		return util.ErrorList{}
 
 	default:
 		panic(ctx.Command())
---
+++ util/errors.go
@@ -0,0 +1,4 @@
+package util
+
+// ErrorList is an alias for []error
+type ErrorList []error
