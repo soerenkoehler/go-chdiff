766ee364b653db92137995f1ce2e117f5aa2e0e3
Sören Köhler <soerenkoehler@users.noreply.github.com>
2021-05-16 00:57:16 +0200

    generic DIgest tests

--- digest/digest_test.go
+++ digest/digest_test.go
@@ -8,22 +8,61 @@ import (
 	// "github.com/google/go-cmp/cmp"
 )
 
-func TestDummy(t *testing.T) {
-	const fileName = "data"
-	const expectedHash = "a6452fbd8c12f8df622c1ca4c567f966801fb56442aca03b4e1303e7a412a9d5"
+type testCase struct {
+	path string
+	size int64
+	seed uint64
+	hash string
+}
 
-	tmpdir := t.TempDir()
-	tmpfile := path.Join(tmpdir, fileName)
+func runDigestCalculationTest(t *testing.T, data []testCase) {
+	root := t.TempDir()
 
-	datautil.CreateRandomFile(tmpfile, 256, 1)
-
-	digest := calculateDigest(tmpdir, "SHA256")
-
-	if digest[fileName].file != fileName {
-		t.Error("Digest map key and DigestEntry.file must match")
+	for _, dataPoint := range data {
+		file := path.Join(root, dataPoint.path)
+		datautil.CreateRandomFile(file, dataPoint.size, dataPoint.seed)
 	}
 
-	if digest[fileName].hash != expectedHash {
-		t.Errorf("expected hash: %s actual hash: %s", expectedHash, digest[fileName].hash)
+	digest := calculateDigest(root, "SHA256")
+
+	if len(digest) != len(data) {
+		t.Fatal("Digest size must match number of input data points")
 	}
+
+	for _, dataPoint := range data {
+		expectedPath := dataPoint.path
+		actualPath := digest[expectedPath].file
+		if actualPath != expectedPath {
+			t.Errorf("DigestEntry.file (%v) must match Digest map key (%v)",
+				actualPath,
+				expectedPath)
+		}
+
+		expectedHash := dataPoint.hash
+		actualHash := digest[expectedPath].hash
+		if actualHash != expectedHash {
+			t.Errorf("actual hash (%v) must match expected hash (%s)",
+				actualHash,
+				expectedHash)
+		}
+	}
+}
+
+func TestDigest(t *testing.T) {
+	runDigestCalculationTest(t, []testCase{{
+		path: "zero",
+		size: 0,
+		seed: 1,
+		hash: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+	}, {
+		path: "data_1",
+		size: 256,
+		seed: 1,
+		hash: "a6452fbd8c12f8df622c1ca4c567f966801fb56442aca03b4e1303e7a412a9d5",
+	}, {
+		path: "sub/data_1",
+		size: 256,
+		seed: 1,
+		hash: "a6452fbd8c12f8df622c1ca4c567f966801fb56442aca03b4e1303e7a412a9d5",
+	}})
 }
