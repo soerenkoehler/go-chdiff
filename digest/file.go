8c636bc62447c25c08258e2763e9de8ee7ca3760
Sören Köhler <soerenkoehler@users.noreply.github.com>
2024-02-07 15:26:03 +0100

    improve error handling

--- digest/file.go
+++ digest/file.go
@@ -15,36 +15,48 @@ type Reader func(digestRootPath, digestFile string) (Digest, error)
 type Writer func(digest Digest, digestFile string) error
 
 func Load(digestPath, digestFile string) (Digest, error) {
+
 	digestFileInfo, err := os.Lstat(digestFile)
 	if err != nil {
+		// hack for better error message under Windows
+		err.(*os.PathError).Op = "lstat"
 		return Digest{}, err
 	}
 
 	digest := NewDigest(digestPath, digestFileInfo.ModTime().Local())
 
 	input, err := os.Open(digestFile)
-	if err == nil {
-		lines := bufio.NewScanner(input)
-		for lines.Scan() {
-			normalized := strings.Replace(lines.Text(), SEPARATOR_TEXT, SEPARATOR_BINARY, 1)
-			println(normalized)
-			tokens := strings.SplitN(normalized, SEPARATOR_BINARY, 2)
-			if len(tokens) != 2 {
-				return Digest{}, fmt.Errorf("invalid digest file")
-			}
-			digest.AddFileHash(tokens[1], tokens[0])
-		}
+	if err != nil {
+		return Digest{}, err
 	}
-	return digest, err
+
+	defer input.Close()
+
+	lines := bufio.NewScanner(input)
+	for lines.Scan() {
+		normalized := strings.Replace(lines.Text(), SEPARATOR_TEXT, SEPARATOR_BINARY, 1)
+		tokens := strings.SplitN(normalized, SEPARATOR_BINARY, 2)
+		if len(tokens) != 2 {
+			return Digest{}, fmt.Errorf("invalid digest file")
+		}
+		digest.AddFileHash(tokens[1], tokens[0])
+	}
+
+	return digest, nil
 }
 
 func Save(digest Digest, digestFile string) error {
 	output, err := os.Create(digestFile)
-	if err == nil {
-		for k, v := range *digest.Entries {
-			fmt.Fprintf(output, "%v%v%v\n", v, SEPARATOR_BINARY, k)
-		}
-		os.Chtimes(digestFile, digest.Location.Time, digest.Location.Time)
+	if err != nil {
+		return err
 	}
+
+	defer output.Close()
+
+	for k, v := range *digest.Entries {
+		fmt.Fprintf(output, "%v%v%v\n", v, SEPARATOR_BINARY, k)
+	}
+	os.Chtimes(digestFile, digest.Location.Time, digest.Location.Time)
+
 	return nil
 }
